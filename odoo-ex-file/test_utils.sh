a87a2ad2301278a1681421f9d069b8b2816d3f4d
thunha14 <painkiller.nhat@gmail.com>
2023-07-21 16:05:27 +0700

    update Unit test

--- odoo-ex-file/test_utils.sh
+++ odoo-ex-file/test_utils.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
  
-/opt/odoo/odoo-bin -c /etc/odoo.conf -d abc -i base_utils,test_base_utils --stop-after-init
+/opt/odoo/odoo-bin -c /etc/odoo.conf -d db -i base_utils,test_base_utils --stop-after-init
